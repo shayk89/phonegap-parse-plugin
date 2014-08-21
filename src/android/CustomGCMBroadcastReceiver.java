0ec90873ff575c43389355b69df8d810005e03bc
Troels Johnsen <troelsjohnsen@gmail.com>
2014-08-21 15:06:37 +0200

    add parseanalytics

--- src/android/CustomGCMBroadcastReceiver.java
+++ src/android/CustomGCMBroadcastReceiver.java
@@ -18,6 +18,7 @@ public class CustomGCMBroadcastReceiver extends GCMBroadcastReceiver {
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.support.v4.app.NotificationCompat;
+import com.parse.PushAnalytics;
 
 import android.content.Intent;
 import android.content.Context;
