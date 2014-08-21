24fe213f53dce78ffb529b0d2d3d8b5f06ca7a15
Troels Johnsen <troelsjohnsen@gmail.com>
2014-08-21 11:24:23 +0200

    new custom receiver

--- src/android/CustomGCMBroadcastReceiver.java
+++ src/android/CustomGCMBroadcastReceiver.java
@@ -2,13 +2,10 @@ package dk.ekstrabladet.gcm;
 
 import android.content.Context;
 
+/*
 import com.google.android.gcm.GCMBroadcastReceiver;
 import static com.google.android.gcm.GCMConstants.DEFAULT_INTENT_SERVICE_CLASS_NAME;
 
-/*
- * Implementation of GCMBroadcastReceiver that hard-wires the intent service to be 
- * com.plugin.gcm.GCMIntentService, instead of your_package.GCMIntentService 
- */
 public class CustomGCMBroadcastReceiver extends GCMBroadcastReceiver {
 	
 	@Override
@@ -16,4 +13,31 @@ public class CustomGCMBroadcastReceiver extends GCMBroadcastReceiver {
     	return "dk.ekstrabladet.gcm" + DEFAULT_INTENT_SERVICE_CLASS_NAME;
     }
 	
+}
+*/
+
+import android.content.Intent;
+import android.content.Context;
+import android.content.BroadcastReceiver;
+
+public class CustomGCMBroadcastReceiver extends BroadcastReceiver {
+private static final String TAG = "CustomGCMBroadcastReceiver";
+ 
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    try {
+      String action = intent.getAction();
+      String channel = intent.getExtras().getString("com.parse.Channel");
+      JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
+ 
+      Log.d(TAG, "got action " + action + " on channel " + channel + " with:");
+      Iterator itr = json.keys();
+      while (itr.hasNext()) {
+        String key = (String) itr.next();
+        Log.d(TAG, "..." + key + " => " + json.getString(key));
+      }
+    } catch (JSONException e) {
+      Log.d(TAG, "JSONException: " + e.getMessage());
+    }
+  }
 }
